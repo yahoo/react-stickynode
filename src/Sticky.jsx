5eec676a9a78b9185e120b2577900d4ed86c1dbc
Kunuk Nykjær <kunukn@gmail.com>
2017-10-06 15:43:18 +0200

    fix bug where custom Modernizr is used without the method prefixed (#89)
    * fix bug where custom Modernizr has been loaded to but does not have the method: prefix
    
    * Update Sticky.jsx
    
     fix bug where custom Modernizr is used without the method prefixed

--- src/Sticky.jsx
+++ src/Sticky.jsx
@@ -326,7 +326,7 @@ class Sticky extends Component {
             winHeight = win.innerHeight || docEl.clientHeight;
             M = window.Modernizr;
             // No Sticky on lower-end browser when no Modernizr
-            if (M) {
+            if (M && M.prefixed) {
                 canEnableTransforms = M.csstransforms3d;
                 TRANSFORM_PROP = M.prefixed('transform');
             }
